58e8dec8d554ab9bd304b39232a73e9130090f8c
Taylor <56216751+devschnee@users.noreply.github.com>
2026-02-27 08:13:54 +0000

    B4344

---
+++ CS/B4344.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace ProgrammingTest
+{
+    internal class B4344
+    {
+        static void Main(string[] args)
+        {
+            int c;
+            c = int.Parse(Console.ReadLine());
+            for(int i = 0; i < c; i++)
+            {
+                string[] s = Console.ReadLine().Split();
+                int[] score = new int[int.Parse(s[0])];
+                for(int j = 0; j < score.Length; j++)
+                {
+                    score[j] = int.Parse(s[j+1]);
+                }
+                int sum = 0;
+                for (int j = 0; j < score.Length; j++)
+                {
+                    sum += score[j];
+                }
+                float avg = sum / score.Length;
+                int cnt = 0;
+                for(int j=0; j < score.Length; j++)
+                {
+                    if(score[j] > avg)
+                    {
+                        cnt++;
+                    }
+                }
+                float ratio = (float)cnt / score.Length * 100;
+                Console.WriteLine($"{ratio:F3}%");
+            }
+        }
+    }
+}
