d1436c42849689ecaf6f1037da0d2b56c1e143e2
HJK <ksturm960@gmail.com>
2026-03-24 13:56:41 +0900

    이진탐색/ 해시

--- py/algorithm/data_structure/binary_search/b1920_s4.py
+++ py/algorithm/data_structure/binary_search/b1920_s4.py
@@ -1,21 +1,5 @@
 #1920 수 찾기
-"""
-import sys
-input = sys.stdin.readline
-N = int(input())
-A = set(map(int, input().split()))  # A를 집합으로 만들어서 탐색 속도를 O(1)로 개선. 리스트는 속도 O(n)로 탐색 함.
 
-M = int(input())
-k = list(map(int, input().split()))  # k는 리스트. 리스트는 순서가 있으므로 인덱스로 접근 가능.
-
-for i in k:
-  if i in A:
-    print(1)
-  else:
-    print(0)
-"""
-
-#이진 탐색 이용해보기
 import sys
 input = sys.stdin.readline
 N = int(input())
